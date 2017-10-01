d942fd5b365f2716d29e8e423c22949cdf055007
Soldiertt <soldiertt@gmail.com>
2017-10-01 11:55:24 +0200

    improve category menu for mobile

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,16 +1,18 @@
-import {Component, OnInit} from "@angular/core";
+import {AfterViewInit, Component, OnInit} from "@angular/core";
 import {Router} from "@angular/router";
 import Category from "../../../model/category.class";
 import {Auth0Service} from "../../../service/auth.service";
 import {I18nService} from "../../../i18n/i18n.service";
 import {DataService} from '../../../service/data.service';
 
+declare var $:any;
+
 @Component({
   selector: 'arth-header',
   templateUrl: 'header.component.html',
   styleUrls: ['header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterViewInit {
   rootCategories: Category[];
   searchTerm: string;
 
@@ -22,6 +24,16 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
     this.dataService.appData.subscribe(appData => {
       this.rootCategories = appData.rootCategories;
+    });
+  }
+
+  ngAfterViewInit() {
+    $(function() {
+      $('.nav-item a').on('click', function(){
+        if($('.navbar-toggler').css('display') !='none'){
+          $(".navbar-toggler").trigger( "click" );
+        }
+      });
     });
   }
 
