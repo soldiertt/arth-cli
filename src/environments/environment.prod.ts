43be9634b674068c70f5529852eb53704a79229e
Soldiertt <soldiertt@gmail.com>
2018-11-15 15:25:55 +0100

    fix url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   restEndpoint: '/api',
   paypalEnvironment: 'production',
-  redirecturl: 'http://www.larthurius.be/authcallback'
+  redirecturl: 'http://www.arthurius.be/authcallback'
 };
